7a520ac2f0e2e3cd4ac0f95ab7f81abbd8597b88
Sunny77D <sondo077@gmail.com>
2022-12-06 13:58:45 -0500

    First commit

---
+++ pages/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+
+const Hero: React.FC = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Hero;
